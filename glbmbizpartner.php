e7a51a6896513099ef561458429cd13d7b8963d9
lehitimasnoel <31946831+lehitimasnoel@users.noreply.github.com>
2023-04-25 20:47:03 +0800

    Add files via upload

---
+++ glbmbizpartner.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class glbmbizpartner extends Model
+{
+    protected $table = 'glbmbizpartner';
+    protected $guarded = []; //means allow all expect the fields mentioned here
+}
