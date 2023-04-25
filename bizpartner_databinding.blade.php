1b057d856583eed3056584b77f0f4ff10d5b45c2
lehitimasnoel <31946831+lehitimasnoel@users.noreply.github.com>
2023-04-25 20:43:10 +0800

    Update bizpartner_databinding.blade.php

--- bizpartner_databinding.blade.php
+++ bizpartner_databinding.blade.php
@@ -21,36 +21,94 @@
 	];
 
 	const bpDataOrganizer = {
-		_filteredById : function(id){
-			filteredBP = [];
-			for(let i=0; i<bizData.length; i++){
-				if(bizData[i].BIZPART_ID == id){
-					filteredBP.push(bizData[i]);
-				}
-			}
-			return filteredBP;
+		_getBpData : async function(){
+			let busyDialog = showBusyDialog("Please wait loading..");
+			busyDialog.open();
+		
+			const response =  await fetch("/bizpartner/getBpData");
+			const data = await response.json();
+			busyDialog.close();
+			return data;
+		},
+		_filteredById : async function(id){
+			let busyDialog = showBusyDialog("Please wait loading..");
+			busyDialog.open();
+			const bp_id = btoa(id);
+			const response =  await fetch("/bizpartner/getDataById/"+bp_id);
+			const dataById = await response.json();
+			busyDialog.close();
+			return dataById;
+			
 		},
 		_updateById : function(id){
 			let busyDialog = showBusyDialog("Please wait loading..");
 				busyDialog.open();
-			
-			bizData.map(bp_id => {
-				if (bp_id.BIZPART_ID == id) {
+				
+			 	let bpDataupdate = [{
 				 
-						bp_id.NAME1    			= ui('BP_TYPE_REGNAME').getValue().trim();
-						bp_id.TYPE     			= ui('BP_TYPE_INFO').getSelectedKey();
-						bp_id.NAME1    			= ui('BP_TYPE_REGNAME').getValue().trim();
-						bp_id.BIZPART_ID      	= ui('INPUT_BP_ID').getValue().trim();
-						bp_id.EXT_PARTNER      	= ui('BP_TYPE_EXTPARTNER').getValue().trim();
-						bp_id.SOURCE_SYS     	= ui('INPUT_CONTROL_INFO_SOURCE_SYS').getValue().trim();
-						bp_id.COMPANY			= ui("BP_COMPANY").getSelectedButton().getId();
-						bp_id.DEL_FLAG    		= ui('CONTROL_INFO_DEL_FLAG').getState();
+						NAME1    			: ui('BP_TYPE_REGNAME').getValue().trim(),
+						TYPE     			: ui('BP_TYPE_INFO').getSelectedKey(),
+						//BIZPART_ID      	: ui('INPUT_BP_ID').getValue().trim(),
+						EXT_PARTNER      	: ui('BP_TYPE_EXTPARTNER').getValue().trim(),
+						SOURCE_SYS     		: ui('INPUT_CONTROL_INFO_SOURCE_SYS').getValue().trim(),
+						COMPANY				: ui("BP_COMPANY").getSelectedButton().getId(),
+						DEL_FLAG    		: ui('CONTROL_INFO_DEL_FLAG').getState()
+				}];
+
+				const data = {
+					BP_ID : id,	
+					bpDataupdate : bpDataupdate
 				}
 				
+			
+			fetch('/bizpartner/update_data',{
+				method : 'POST',
+				headers : {
+					'Content-Type' : 'application/json',
+					'X-CSRF-TOKEN': $('meta[name="csrf-token"]').attr('content')
+				},
+				body : JSON.stringify(data)
+			}).then((response) => {
+				if(response.ok){
+					screenMode._display(id);
+					fn_show_message_toast("Successfully updated business partner #"+id);
+				}
+				console.log(response);
+				return response.json();
+			}).then(data => {
+				console.log(data);
+				busyDialog.close();
+			}).catch((err) => {
+				console.log("Rejected "+err);
+				busyDialog.close();
 			});
-			screenMode._display(id);
-			listingBp._getData(bizData);
-			setTimeout(() => {busyDialog.close();}, 2000);
+		},
+		_removeById : function(id){
+			let busyDialog = showBusyDialog("Please wait loading..");
+				busyDialog.open();
+				
+			const bp_id = {BP_ID : id}
+			fetch('/bizpartner/removeDataById',{
+				method : 'POST',
+				headers : {
+					'Content-Type' : 'application/json',
+					'X-CSRF-TOKEN': $('meta[name="csrf-token"]').attr('content')
+				},
+				body : JSON.stringify(bp_id)
+			}).then((response) => {
+				if(response.ok){
+					fn_show_message_toast("Successfully deleted business partner #"+id);
+					ui('LEFT_MENU_TEMPLATE-MENU_LIST-2').firePress();
+				}
+				console.log(response);
+				return response.json();
+			}).then(data => {
+				console.log(data);
+				busyDialog.close();
+			}).catch((err) => {
+				console.log("Rejected "+err);
+				busyDialog.close();
+			});
 		},
 		_getRadioIndex : function(id){
 			let radioButton = ui("BP_COMPANY").getButtons();
@@ -89,6 +147,8 @@
 			ui("CREATE_BP_SAVE_BTN").setVisible(true);
 			ui("CREATE_BP_EDIT_BTN").setVisible(false);
 			ui("CREATE_BP_CANCEL_BTN").setVisible(false);
+			ui("CREATE_BP_DEL_BTN").setVisible(false);
+			
 
 			//title and crumbs
 			ui('BP_TITLE').setText(bp_title);
@@ -112,6 +172,7 @@
 			ui("CREATE_BP_SAVE_BTN").setVisible(true);
 			ui("CREATE_BP_EDIT_BTN").setVisible(false);
 			ui("CREATE_BP_CANCEL_BTN").setVisible(true);
+			ui("CREATE_BP_DEL_BTN").setVisible(false);
 
 			//Fields
 			ui('BP_TYPE_INFO').setEditable(true);
@@ -133,28 +194,35 @@
 			ui("CREATE_BP_SAVE_BTN").setVisible(false);
 			ui("CREATE_BP_EDIT_BTN").setVisible(true);
 			ui("CREATE_BP_CANCEL_BTN").setVisible(false);
+			ui("CREATE_BP_DEL_BTN").setVisible(true);
 
 
 			//fields with value
-			let data = bpDataOrganizer._filteredById(id);
-			if(data.length > 0){
-				ui('BP_TYPE_INFO').setSelectedKey(data[0].TYPE).setEditable(false);
-       			ui('BP_TYPE_REGNAME').setValue(data[0].NAME1).setEditable(false);
-        		ui('INPUT_BP_ID').setValue(data[0].BIZPART_ID).setEditable(false);
-				ui('BP_TYPE_EXTPARTNER').setValue(data[0].EXT_PARTNER).setEditable(false);
-				ui('INPUT_CONTROL_INFO_SOURCE_SYS').setValue(data[0].SOURCE_SYS).setEditable(false);
-				let radioIndex = bpDataOrganizer._getRadioIndex(data[0].COMPANY);
-				ui('BP_COMPANY').setSelectedIndex(radioIndex).setEditable(false);
-				ui('CONTROL_INFO_DEL_FLAG').setState(data[0].DEL_FLAG).setEnabled(false);
-			
-			
-				//title and crumbs
-				ui('BP_TITLE').setText(bp_title);
-				ui('CREATE_BP_BRDCRMS').setCurrentLocationText(bp_title);
-				ui("PANEL_FORM").setTitle("Business Partner ID "+"("+data[0].BIZPART_ID+")");
+			let response =  async () => {
+				let data = await bpDataOrganizer._filteredById(id);
+				console.log(data);
+				if(data.length > 0){
+					ui('BP_TYPE_INFO').setSelectedKey(data[0].TYPE).setEditable(false);
+					ui('BP_TYPE_REGNAME').setValue(data[0].NAME1).setEditable(false);
+					ui('INPUT_BP_ID').setValue(data[0].BIZPART_ID).setEditable(false);
+					ui('BP_TYPE_EXTPARTNER').setValue(data[0].EXT_PARTNER).setEditable(false);
+					ui('INPUT_CONTROL_INFO_SOURCE_SYS').setValue(data[0].SOURCE_SYS).setEditable(false);
+					let radioIndex = bpDataOrganizer._getRadioIndex(data[0].COMPANY);
+					ui('BP_COMPANY').setSelectedIndex(radioIndex).setEditable(false);
+					let delFlagBool = (data[0].DEL_FLAG) ? true : false;
+					ui('CONTROL_INFO_DEL_FLAG').setState(delFlagBool).setEnabled(false);
+				
+				
+					//title and crumbs
+					ui('BP_TITLE').setText(bp_title);
+					ui('CREATE_BP_BRDCRMS').setCurrentLocationText(bp_title);
+					ui("PANEL_FORM").setTitle("Business Partner ID "+"("+data[0].BIZPART_ID+")");
 
-				go_App_Right.to('CREATE_BP_PAGE');
-			}			
+					go_App_Right.to('CREATE_BP_PAGE');
+				}	
+			};
+			response();
+					
 		},
 		_clear : function(){
 			ui('MESSAGE_STRIP_BP_ERROR').destroyContent().setVisible(false);
@@ -181,29 +249,28 @@
 			COMPANY				: ui("BP_COMPANY").getSelectedButton().getId(),
 			DEL_FLAG    		: ui('CONTROL_INFO_DEL_FLAG').getState()
    		};
-		//add new data to array
-		bizData.push(data_for_general);
-		screenMode._display(data_for_general.BIZPART_ID);
-		setTimeout(() => {busyDialog.close();}, 2000);
 		
-		//commented use for backend only
-		/*fetch('/bizpartner/create_data',{
+		fetch('/bizpartner/create_data',{
 			method : 'POST',
 			headers : {
 				'Content-Type' : 'application/json',
 				'X-CSRF-TOKEN': $('meta[name="csrf-token"]').attr('content')
 			},
 			body : JSON.stringify(data_for_general)
-
-
 		}).then((response) => {
+			if(response.ok){
+				screenMode._display(data_for_general.BIZPART_ID);
+				fn_show_message_toast("Successfully created new business partner #"+data_for_general.BIZPART_ID);
+			}
 			console.log(response);
 			return response.json();
 		}).then(data => {
 			console.log(data);
+			busyDialog.close();
 		}).catch((err) => {
 			console.log("Rejected "+err);
-		});*/
+			busyDialog.close();
+		});
         
     }
 
@@ -211,14 +278,11 @@
 		
 		_get_data(search){
 			
-			let busyDialog = showBusyDialog("Please wait loading..");
-				busyDialog.open();
-
-				let data = bpDataOrganizer._filteredById(search);
+			let response = async () => {
+				let data =  await bpDataOrganizer._filteredById(search);
 				this._bind_data(data);
-			
-			
-			setTimeout(() => {busyDialog.close();}, 2000);
+			};
+			response();		
 		},
 		_bind_data(data){
 		
@@ -280,6 +344,33 @@
 			new sap.m.HBox({
 				items:[
 				new sap.m.Label({text:"Confirm to save changes?"})
+				]
+			})
+		]
+	}).addStyleClass('sapUiSizeCompact');
+	let lv_dialog_del = new sap.m.Dialog("BP_DELETE_DIALOG",{
+		title: "Confirmation",
+		beginButton: new sap.m.Button({
+			text:"Ok",
+			type:"Accept",
+			icon:"sap-icon://accept",
+			press:function(oEvt){
+				bpDataOrganizer._removeById(screenMode._id);
+				oEvt.getSource().getParent().close();
+			}
+		}),
+		endButton:new sap.m.Button({
+			text:"Cancel",
+			type:"Reject",
+			icon:"sap-icon://decline",
+			press:function(oEvt){
+			oEvt.getSource().getParent().close();
+			}
+		}),
+		content:[
+			new sap.m.HBox({
+				items:[
+				new sap.m.Label({text:"Confirm to delete business partner?"})
 				]
 			})
 		]
